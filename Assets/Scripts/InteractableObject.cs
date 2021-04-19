b3f4872db46739281c92da4fad62b296b6c3879e
unknown <stefanie.zwingenberger03@htl-salzburg.ac.at>
2021-04-19 08:36:48 +0200

    interactable Object

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractableObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnMouseDown()
+    { 
+        this.gameObject.active = false;
+        GameObject.Find("ObjectName").GetComponent<Text>().text = this.gameObject.name;
+        Debug.Log("click");
+    }
+
+
+    
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,15 +6,18 @@ public class Player : MonoBehaviour
 {
     public Vector3 mousePos;
     public Vector3 mouseCameraPos;
-    public float speed = 0.1f;
+    public float speed = 0.3f;
+    private Vector3 playerPos;
 
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
             mousePos = Input.mousePosition;
-            mouseCameraPos = Camera.main.ScreenToWorldPoint(mousePos);
-            this.transform.position = Vector3.MoveTowards(this.transform.position,mouseCameraPos,speed);
+            var newPos = Camera.main.ScreenToWorldPoint(mousePos);
+            playerPos = new Vector3(newPos.x, newPos.y, this.gameObject.transform.position.z);
+
         }
+            this.transform.position = Vector3.MoveTowards(this.transform.position,playerPos,speed);
     }
 }
